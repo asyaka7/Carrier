79be3a7d1b1d0cf41b8ad75cfa5be98b373aabc4
asyaka7 <anastasiya.lyskavets@gmail.com>
2024-02-09 22:23:10 +0400

    Update rotator
    Updated rotation for moving rocks.

--- Assets/Code/Scripts/Rotator.cs
+++ Assets/Code/Scripts/Rotator.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     [SerializeField] float speed = 3;
+    [SerializeField] float maxAngle = 45;
+
+    float currenAngle = 0;
+    float directionFactor = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currenAngle = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, speed * Time.deltaTime, 0);
+        float deltaAngle = speed * Time.deltaTime * directionFactor;
+        currenAngle += deltaAngle;
+        transform.Rotate(0, 0, deltaAngle);
+
+        if (Mathf.Abs(currenAngle) > maxAngle)
+        {
+            //currenAngle = 0;
+            directionFactor *= -1;
+        }
     }
 }
