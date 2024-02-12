ec3c6b9959db31882f5c8259a114b76c43eabf49
asyaka7 <anastasiya.lyskavets@gmail.com>
2024-02-12 04:16:52 +0400

    Change level design
    Added moving peaks.
    Changed rotated rocks.
    Added more details for transitioning scene.
    Added more life-chargers.

--- Assets/Code/Scripts/Rotator.cs
+++ Assets/Code/Scripts/Rotator.cs
@@ -10,13 +10,11 @@ public class Rotator : MonoBehaviour
     float currenAngle = 0;
     float directionFactor = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
         currenAngle = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float deltaAngle = speed * Time.deltaTime * directionFactor;
@@ -25,7 +23,6 @@ public class Rotator : MonoBehaviour
 
         if (Mathf.Abs(currenAngle) > maxAngle)
         {
-            //currenAngle = 0;
             directionFactor *= -1;
         }
     }
